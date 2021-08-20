cc42118c3b25f81c1da2b6a50c53cd9eeea240bb
ywjawmw <38470046+ywjawmw@users.noreply.github.com>
2021-08-20 15:46:15 +0800

    add train file, add cifar 10 dataset for trainning
    add cifar 10 dataset for trainning

--- experience/图像分类/pytorch-res2net-master/train.py
+++ experience/图像分类/pytorch-res2net-master/train.py
@@ -184,7 +184,7 @@ def main():
         ])
         transform_test = transforms.Compose([
             transforms.ToTensor(),
-            transforms.Normalize((0.485, 0.456, 0.406),
+            transforms.Normalize((0.485, 0.456, 0.406),            # 按照官方给出的进行归一化
                                  (0.229, 0.224, 0.225)),
         ])
 
